563bfd98f4c628a604e4cb317e252ccbc86033f0
kaalm1 <kaalm1@gmail.com>
2017-06-08 14:21:31 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.chars.last == '.'
   end
 
   def question?
-
+    self.chars.last == '?'
   end
 
   def exclamation?
-
+    self.chars.last == '!'
   end
 
   def count_sentences
-
+    sentences = self.split(/[.!?]/)
+    sentences.select {|sentence| sentence != ''}.length
   end
 end
